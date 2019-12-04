204596a169e19843865847b7869ae076e936c1aa
sndnyangd <sndnyangd@gmail.com>
2019-12-04 16:49:52 -0500

    update the js file

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,10 @@
 function update_port(obj) {
     let port = $(obj).val();
 
-    let div_list = $(".iframe_container");
+    let div_list = $("iframe");
     for (let i in div_list) {
         let div = $(div_list[i]);
         let pid = div.id;
-        div.html('<iframe src="http://localhost:' + port + '/#scalars&regexInput=' + pid + '" frameborder="0"></iframe>');
+        div.attr('src', "http://localhost:" + port + "/#scalars&regexInput=" + pid);
     }
 }
